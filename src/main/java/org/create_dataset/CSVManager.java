5e977cf4b13bd869d289e4e13439be87f2d6a6e6
Mattia9727 <mattiaantonangeli@gmail.com>
2024-02-18 09:40:24 +0100

    Something strange happened with CSVManager. Rollback.

--- src/main/java/org/create_dataset/CSVManager.java
+++ src/main/java/org/create_dataset/CSVManager.java
@@ -10,20 +10,17 @@ import java.util.List;
 
 public class CSVManager {
 
-    public void createCSVWithHeader(String projName) throws IOException {
+    public void generateCSVFromVersions(String projName, List<VersionRelease> versions) throws IOException {
         File csvOutput = new File(projName+".csv");
         FileUtils.writeStringToFile(csvOutput, "version;oldfilename;newfilename;loc;loc_touched;loc_added;loc_removed;max_loc_added;avg_loc_added;churn;max_churn;avg_churn;NR;nAuthors;buggy\n", false);
-    }
 
-    public void addVersionsDataToCSV(String projName, List<VersionRelease> versionReleases) throws IOException {
-
-        for (int i = 1; i< versionReleases.size()-1; i++){
-            for (HashDifference h : versionReleases.get(i).getHashDiffs()){
-                FileUtils.writeStringToFile(csvOutput, versionReleases.get(i).getName() + ";" + h.getOldClassName() + ";" +
-                    h.getNewClassName() + ";" +
-                    h.getLines() + ";" + h.getLocTouched() + ";" +
-                    h.getAddedLines() + ";" + h.getRemovedLines() + ";" +
-                    h.getMaxLocAdded() + ";" + h.getAvgLocAdded() + ";" + h.getChurn() + ";" + h.getMaxChurn()
+        for (int i=1; i<versions.size()-1; i++){
+            for (HashDifference h : versions.get(i).getHashDiffs()){
+                FileUtils.writeStringToFile(csvOutput, versions.get(i).getName() + ";" + h.getOldClassName() + ";" +
+                        h.getNewClassName() + ";" +
+                        h.getLines() + ";" + h.getLocTouched() + ";" +
+                        h.getAddedLines() + ";" + h.getRemovedLines() + ";" +
+                        h.getMaxLocAdded() + ";" + h.getAvgLocAdded() + ";" + h.getChurn() + ";" + h.getMaxChurn()
                         + ";" + h.getAvgChurn() + ";" + h.getNumberOfRevisions() + ";" + h.getnAuthors() + ";" + h.isBuggy() + "\n", true);
             }
         }
